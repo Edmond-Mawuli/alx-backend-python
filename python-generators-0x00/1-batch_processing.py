854488d1c65163a646ebc5d330e28504c43b6ce8
Edmond-Mawuli <edmondasoe123@gmail.com>
2025-09-13 13:12:53 +0000

    Update 1-batch_processing.py

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -17,11 +17,11 @@ def stream_users_in_batches(batch_size):
         cursor = connection.cursor(dictionary=True)
         cursor.execute("SELECT * FROM user_data;")
 
-        while True:
+        while True:  # loop 1
             batch = cursor.fetchmany(batch_size)
             if not batch:
                 break
-            yield batch  # yield the entire batch as a list of rows
+            yield batch   # ✅ return batches using yield
 
     except mysql.connector.Error as err:
         print(f"Error: {err}")
@@ -34,9 +34,9 @@ def stream_users_in_batches(batch_size):
 
 def batch_processing(batch_size):
     """
-    Processes users in batches, filtering only users older than 25
+    Generator that yields users over 25 years old
     """
-    for batch in stream_users_in_batches(batch_size):   # loop 1
-        for user in batch:                             # loop 2
-            if user['age'] > 25:                       # filtering
-                yield user
+    for batch in stream_users_in_batches(batch_size):   # loop 2
+        for user in batch:                             # loop 3
+            if user['age'] > 25:
+                yield user   # ✅ yield instead of print
