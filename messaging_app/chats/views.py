0917f0d4919203df87d5a62c30a25943fa2533b3
Edmond-Mawuli <edmondasoe123@gmail.com>
2025-09-21 01:54:00 +0000

    Update views.py

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,14 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status, filters
+from rest_framework.response import Response
+from .models import User, Conversation, Message
+from .serializers import UserSerializer, ConversationSerializer, MessageSerializer
 
-# Create your views here.
-from rest_framework import viewsets
-from .models import Conversation, Message
-from .serializers import ConversationSerializer, MessageSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["username", "email", "phone_number"]
+
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
 
+
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+
+    def create(self, request, *args, **kwargs):
+        """Custom create method to attach sender automatically."""
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
