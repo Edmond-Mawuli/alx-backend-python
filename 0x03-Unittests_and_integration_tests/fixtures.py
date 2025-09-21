321d196d78e6ace1c6cab5ecb47a4f4e409e4cb8
Edmond-Mawuli <edmondasoe123@gmail.com>
2025-09-21 00:14:35 +0000

    Update fixtures.py

--- 0x03-Unittests_and_integration_tests/fixtures.py
+++ 0x03-Unittests_and_integration_tests/fixtures.py
@@ -1,34 +1,16 @@
 #!/usr/bin/env python3
-"""
-Fixtures module containing payloads for unit and integration tests.
-"""
+"""Fixtures for unit tests"""
 
-# Payload returned for the organization endpoint
 org_payload = {
     "login": "google",
-    "repos_url": "https://api.github.com/orgs/google/repos",
-    "id": 1342004,
+    "id": 1,
+    "repos_url": "https://api.github.com/orgs/google/repos"
 }
 
-# Payload returned for the repos endpoint
 repos_payload = [
-    {
-        "id": 7697149,
-        "name": "episodes.dart",
-        "owner": {"login": "google", "id": 1342004},
-        "private": False,
-        "license": {"key": "apache-2.0", "name": "Apache License 2.0"},
-    },
-    {
-        "id": 7776515,
-        "name": "cpp-netlib",
-        "owner": {"login": "google", "id": 1342004},
-        "private": False,
-    },
+    {"id": 1, "name": "repo1"},
+    {"id": 2, "name": "repo2"},
+    {"id": 3, "name": "repo3"},
 ]
 
-# Expected list of repo names
-expected_repos = ["episodes.dart", "cpp-netlib"]
-
-# Subset of repos licensed under Apache 2.0
-apache2_repos = ["episodes.dart"]
+expected_repos = ["repo1", "repo2", "repo3"]
