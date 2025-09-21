fe5fb6105d80b2834cc829860e53b7fba56d03e0
Edmond-Mawuli <edmondasoe123@gmail.com>
2025-09-21 00:20:17 +0000

    Update fixtures.py

--- 0x03-Unittests_and_integration_tests/fixtures.py
+++ 0x03-Unittests_and_integration_tests/fixtures.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
-"""Fixtures for unit tests"""
+"""Fixtures for integration and unit tests"""
 
 org_payload = {
-    "login": "google",
+    "login": "test",
     "id": 1,
-    "repos_url": "https://api.github.com/orgs/google/repos"
+    "repos_url": "https://api.github.com/orgs/test/repos"
 }
 
 repos_payload = [
-    {"id": 1, "name": "repo1"},
-    {"id": 2, "name": "repo2"},
-    {"id": 3, "name": "repo3"},
+    {"id": 101, "name": "repo1", "license": {"key": "mit"}},
+    {"id": 102, "name": "repo2", "license": {"key": "apache-2.0"}},
+    {"id": 103, "name": "repo3", "license": {"key": "gpl-3.0"}},
 ]
 
+# List of just the repo names
 expected_repos = ["repo1", "repo2", "repo3"]
+
+# Filtered repos with apache2 license
+apache2_repos = ["repo2"]
